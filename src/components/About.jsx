b23cbe41ec249eb64e45bc4e3ce9656550d09b16
David Athobe <dathobek@gmail.com>
2018-07-17 00:14:54 +0100

    !news!

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,6 +11,11 @@ export default class About extends Component{
                 <Col xm={12} sm={8} smOffset={2}>
                  <Image src="assets/davidA.jpeg" className="about-profile-pic" rounded/>
                  <h3>D Boy</h3>
+                 <p>The most important thing in the world is family and love. ..</p>
+                 <p>I am blessed to have so many great things in my life - family, friends and God. All will be in my thoughts daily.</p>
+                 <p>You need a strong family because at the end, they will love you and support you unconditionally. Luckily, I have two Sons,two Daughters,A wife, mom and sisters.</p>
+                 <p>A real man loves his wife, and places his family as the most important thing in life. Nothing has brought me more peace and content in life than simply being a good husband and father.</p>
+                 <p>Family always gonna be there. The material things, they come and go.</p>
                 </Col>
               </Grid>     
             </div>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,26 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {Grid,Row,Col,Image} from 'react-bootstrap';
+import './News.css';
+
+
 
 export default class News extends Component{
     render(){
         return(
             <div>
-                NEWS 
+              <Image src="assets/star.jpg" className="display"/>
+              <Grid>
+                <h2>NEWS</h2>  
+                <Row>
+                  <Col xm={12} sm={8}>
+
+                  </Col>
+                  <Col xm={12} sm={4}>
+                  <Image src="" className=""/>
+
+                  </Col>
+                </Row>   
+              </Grid>    
             </div>
         )
     }
