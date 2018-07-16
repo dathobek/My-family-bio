5ef868b56134905907daef21de9f885bd568bea2
David Athobe <dathobek@gmail.com>
2018-07-16 18:07:07 +0100

    about-jsx-css-!!-!!

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Image, Grid, Col} from 'react-bootstrap';
+import './About.css';
 
 export default class About extends Component{
     render(){
         return(
             <div>
-                ABOUT PAGE
+               <Image src="assets/all.jpeg" ClassName="header-image"/> 
+              <Grid>
+                <Col xm={12} sm={8} smOffset={2}>
+                 <Image src="assets/davidA.jpeg" ClassName="about-profile-pic" rounded/>
+                 <h3>D Boy</h3>
+                </Col>
+              </Grid>     
             </div>
         )
     }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,17 +17,17 @@ export default class Home extends Component{
                 </Jumbotron> 
                <Row className="show-grid text-center">
                    <Col xs={12} sm={4} className="person-wrapper">
-                     <Image src = "assests/davidA.jpeg" circle className="profile-pic" />
+                     <Image src = "assets/davidA.jpeg" circle className="profile-pic" />
                      <h3>David</h3>
                      <p>There isn't a rule. You just practice and find out which way works best for you.In your world you have total and absolute power..</p>
                    </Col>
                    <Col xs={12} sm={4} className="person-wrapper">
-                     <Image src = "assests/Faith-nd-Abi.jpeg" circle className="profile-pic" />
+                     <Image src = "assets/Faith-nd-Abi.jpeg" circle className="profile-pic" />
                      <h3>Faith Nd Abigail</h3>
                      <p>Let your heart take you to wherever you want to be..Always Smile</p>
                    </Col>
                    <Col xs={12} sm={4} className="person-wrapper">
-                     <Image src = "assests/emma.jpeg" circle className="profile-pic" />
+                     <Image src = "assets/emma.jpeg" circle className="profile-pic" />
                      <h3>Emmanuel</h3>
                      <p>It looks so good, I might as well not stop. Just a happy little shadow that lives in there. It's amazing what you can do with a little love in your heart...</p>
                    </Col>
