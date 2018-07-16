bf084a58fd0fd46c0634bbba7fa423ddca07785e
David Athobe <dathobek@gmail.com>
2018-07-16 11:20:55 +0100

    Nav-css

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Navbar, Nav, Navitem} from 'react-bootstrap';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default class CustomNavbar extends Component{
@@ -14,15 +14,15 @@ export default class CustomNavbar extends Component{
                   </Navbar.Header>  
                 <Navbar.Collapse>
                    <Nav pullRight>
-                     <Navitem eventKey = {1} componentClass = {Link} to ="">
-                        Home
-                     </Navitem>  
-                     <Navitem eventKey = {2} componentClass = {Link} to ="/about">
+                     <NavItem eventKey = {1} componentClass = {Link} to ="">  
+                      Home
+                     </NavItem>  
+                     <NavItem eventKey = {2} componentClass = {Link} to ="/about">
                         About
-                     </Navitem> 
-                     <Navitem eventKey = {3} componentClass = {Link} to ="/news">
+                     </NavItem> 
+                     <NavItem eventKey = {3} componentClass = {Link} to ="/news">
                         News
-                     </Navitem>   
+                     </NavItem>   
                    </Nav>
                 </Navbar.Collapse>            
             </Navbar>
