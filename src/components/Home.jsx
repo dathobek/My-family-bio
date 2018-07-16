428ca74e2d6adb2932b9eed6993dfa332cd71b7c
David Athobe <dathobek@gmail.com>
2018-07-16 17:51:20 +0100

    !-

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,19 +17,19 @@ export default class Home extends Component{
                 </Jumbotron> 
                <Row className="show-grid text-center">
                    <Col xs={12} sm={4} className="person-wrapper">
-                     <Image src = "assests/david.jpeg" circle className="profile-pic" />
+                     <Image src = "assests/davidA.jpeg" circle className="profile-pic" />
                      <h3>David</h3>
-                     <p>There isn't a rule. You just practice and find out which way works best for you. See there, told you that would be easy. I thought today we would do a happy little picture.</p>
+                     <p>There isn't a rule. You just practice and find out which way works best for you.In your world you have total and absolute power..</p>
                    </Col>
                    <Col xs={12} sm={4} className="person-wrapper">
-                     <Image src = "assests/david.jpeg" circle className="profile-pic" />
-                     <h3>David</h3>
-                     <p>There isn't a rule. You just practice and find out which way works best for you. See there, told you that would be easy. I thought today we would do a happy little picture.</p>
+                     <Image src = "assests/Faith-nd-Abi.jpeg" circle className="profile-pic" />
+                     <h3>Faith Nd Abigail</h3>
+                     <p>Let your heart take you to wherever you want to be..Always Smile</p>
                    </Col>
                    <Col xs={12} sm={4} className="person-wrapper">
-                     <Image src = "assests/david.jpeg" circle className="profile-pic" />
-                     <h3>David</h3>
-                     <p>There isn't a rule. You just practice and find out which way works best for you. See there, told you that would be easy. I thought today we would do a happy little picture.</p>
+                     <Image src = "assests/emma.jpeg" circle className="profile-pic" />
+                     <h3>Emmanuel</h3>
+                     <p>It looks so good, I might as well not stop. Just a happy little shadow that lives in there. It's amazing what you can do with a little love in your heart...</p>
                    </Col>
                </Row>
                
