48bcb92097bee350c923e936c0adb265f8724951
David Athobe <dathobek@gmail.com>
2018-07-16 17:20:05 +0100

    Home-jsx-css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,10 +11,31 @@ export default class Home extends Component{
                 <Jumbotron>
                     <h1>Welcome To Athobe's Family</h1>
                     <p>This is God's Family</p>
-                </Jumbotron> 
-                <Link to="/About">
+                 <Link to="/About">
                    <Button bsStyle="primary">About</Button>
-                </Link>
+                 </Link>  
+                </Jumbotron> 
+               <Row className="show-grid text-center">
+                   <Col xs={12} sm={4} className="person-wrapper">
+                     <Image src = "assests/david.jpeg" circle className="profile-pic" />
+                     <h3>David</h3>
+                     <p>There isn't a rule. You just practice and find out which way works best for you. See there, told you that would be easy. I thought today we would do a happy little picture.</p>
+                   </Col>
+                   <Col xs={12} sm={4} className="person-wrapper">
+                     <Image src = "assests/david.jpeg" circle className="profile-pic" />
+                     <h3>David</h3>
+                     <p>There isn't a rule. You just practice and find out which way works best for you. See there, told you that would be easy. I thought today we would do a happy little picture.</p>
+                   </Col>
+                   <Col xs={12} sm={4} className="person-wrapper">
+                     <Image src = "assests/david.jpeg" circle className="profile-pic" />
+                     <h3>David</h3>
+                     <p>There isn't a rule. You just practice and find out which way works best for you. See there, told you that would be easy. I thought today we would do a happy little picture.</p>
+                   </Col>
+               </Row>
+               
+                
+                
+                
             </Grid>    
         )
     }
