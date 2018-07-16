6ee485d6738a10c83af003b300d45555f7747d94
David Athobe <dathobek@gmail.com>
2018-07-16 10:18:51 +0100

    Home/About

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css'
+
+
+export default class Home extends Component{
+    render(){
+        return (
+            <Grid>
+                <Jumbotron>
+                    <h1>Welcome To Athobe's Family</h1>
+                    <p>This is the God's Family</p>
+                </Jumbotron> 
+                <Link to="/About">
+                   <Button> bsStyle="primary">About</Button>
+                </Link>
+            </Grid>    
+        )
+    }
+
+}
